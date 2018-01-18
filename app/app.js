91e34c24ca5f0fdf84b2d0f7ba731d947f0aac45
Alexander Choy <alexanderchoy18@gmail.com>
2018-01-18 00:49:44 -0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import {Button, Icon} from 'react-materialize'
+
+import Home from './components/Home.js';
+
+ReactDom.render(<Home />, document.getElementById('app'));
---
+++ app/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {Button, Icon} from 'react-materialize'
+
+const Home = () => (
+  <div>
+    <nav>
+      <h1>GitHub Consulting</h1>
+      <ul>
+        <li>Navigation</li>
+        <li>Navigation</li>
+        <li>Navigation</li>
+      </ul>
+    </nav>
+    <h1>Pruebas</h1>
+    <Button waves='light'>
+      <Icon>thumb_up</Icon>
+    </Button>
+  </div>
+)
+
+export default Home;
